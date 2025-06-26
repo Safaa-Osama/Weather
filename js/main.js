7eb3037dcfa0b317e760ba5ec15e8757d08a478e
safaa-osama <safaaosama386@gmail.com>
2025-06-26 18:43:25 +0300

    last one

--- js/main.js
+++ js/main.js
@@ -1,79 +1,84 @@
-
-var baseUrl = "http://api.weatherapi.com/v1/";
+var baseUrl = "http://api.weatherapi.com/v1/forecast.json";
 var APIKey = "7814b0fab99046d586e135005252406";
-// var cWeather= [];
+var findInput = document.querySelector("#findInput");
 var findBtn = document.getElementById("findBtn");
-var searchLocation = '';
 
-async function getCurrentWeather(endPoint,city) {
-    var response = await fetch(`${baseUrl}${endPoint}?key=${APIKey}&q=${city}&days=3`);
-    var cWeather = await response.json();
-    console.log(cWeather);
-    displayWeather(cWeather);
+async function getCurrentWeather(city) {
+  var response = await fetch(
+    `${baseUrl}?key=${APIKey}&q=${city}&days=3`
+  );
+  var cWeather = await response.json();
+  console.log(cWeather);
+  displayWeather(cWeather);
 }
 
-getCurrentWeather('forecast.json','london');
+getCurrentWeather("alexandria");
 
-function displayWeather(curentWeather){
+function displayWeather(curentWeather) {
+  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  var today = new Date(curentWeather.current.last_updated);
+  var todayName = days[today.getDay()];
+  var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  var dayOfMonth = today.getDate();
+  var monthName = months[today.getMonth()];
+  var forecast = curentWeather.forecast.forecastday;
+
+  var weather = ``;
+
+  weather += `
+    <div class="col-md-12 col-lg-4 bg-dark p-2">
+      <div class="header d-flex justify-content-between bg-black">
+        <span>${todayName}</span>
+        <span>${dayOfMonth} ${monthName}</span>
+      </div>
+      <div id="location">
+        <h5 class="my-3">${curentWeather.location.name}</h5>
+      </div>
+      <div class="d-flex flex-column justify-content-center" id="tempreture">
+        <h1 class="my-3">${curentWeather.current.temp_c} °C</h1>
+        <img src="${curentWeather.current.condition.icon}" class="sunny" alt="condition">
+        <span>${curentWeather.current.condition.text}</span>
+        <div class="icons mt-5">
+          <img src="./img/icon-umberella.png" alt="umbrella">
+          <span>${curentWeather.current.humidity}%</span>
+          <img src="./img/icon-wind.png" alt="wind">
+          <span>${curentWeather.current.wind_kph} kph</span>
+          <img src="./img/icon-compass.png" alt="compass">
+          <span>${curentWeather.current.wind_dir}</span>
+        </div>
+      </div>
+    </div>`;
+
   
-var lastUpdated = new Date(curentWeather['current'].last_updated);
-var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var todayName = days[lastUpdated.getDay()];
+  for (var i = 1; i < 3; i++) {
+    var date = new Date(forecast[i].date);
+    var nameOfDay = days[date.getDay()];
 
-var months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-var dayOfMonth = lastUpdated.getDate();
-var monthName = months[lastUpdated.getMonth()];
-
-
-var weather = ``;
-        weather = ` <div class="col-md-12 col-lg-4 bg-dark p-2">
-
-        <div class="header d-flex justify-content-between bg-black" id="">
-          <span>${todayName}</span>
-          <span>${dayOfMonth}${monthName}</span>
-        </div>
-        <div id="location">
-          <h5 class="my-3">${curentWeather['location'].name}</h5>
-        </div>
-        <div class="d-flex flex-column justify-content-center" id="tempreture">
-          <h1 class="my-3">${curentWeather['current'].temp_c}C</h1>
-          <img src="${curentWeather.current.condition.icon}" class="sunny" alt="condition">
-
-          <span>${curentWeather['current'].condition.text}</span>
-          <div class="icons mt-5">
-
-            <img src="./img/icon-umberella.png" alt="umbrella">
-            <span>20%</span>
-            <img src="./img/icon-wind.png" alt="wind">
-            <span>${curentWeather['current'].wind_kph}+kph</span>
-            <img src="./img/icon-compass.png" alt="compass">
-            <span>East</span>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-md-12 col-lg-4 bg-black ">
-        <div class="header text-center bg-dark" id="">
-          <span>Sunday</span>
+    weather += `
+      <div class="col-md-12 col-lg-4 ${i==1?'bg-black':'bg-dark'} text-center">
+        <div class="header ${i==1?'bg-dark':'bg-black'}">
+          <span>${nameOfDay}</span>
         </div>
         <div class="d-flex flex-column justify-content-between my-5 align-items-center">
-          <h3>32.5</h3>
-          <span>24</span> <span>sunny</span>
+          <img src="${forecast[i].day.condition.icon}" alt="icon">
+          <h3>${forecast[i].day.maxtemp_c} °C</h3>
+          <span>${forecast[i].day.mintemp_c} °C</span>
+          <span>${forecast[i].day.condition.text}</span>
         </div>
-      </div>
+      </div>`;
+  }
 
-        <div class="col-md-12 col-lg-4 bg-dark ">
-        <div class="header text-center bg-black" id="">
-          <span>Tuesday</span>
-        </div>
-        <div class="d-flex flex-column justify-content-between my-5 align-items-center">
-          <h3>32.5</h3>
-          <span>24</span> <span>sunny</span>
-        </div>
-      </div>`
-    document.getElementById('rowData').innerHTML = weather;
+  document.getElementById("rowData").innerHTML = weather;
 }
 
-function search(term){
-
-}
+findInput.addEventListener('input', function(e){
+    if (findInput.value) {
+        getCurrentWeather(findInput.value);
+    }
+});
+//OR
+findBtn.addEventListener('click', function(e) {
+    if (findInput.value) {
+        getCurrentWeather(findInput.value);
+    }
+});
